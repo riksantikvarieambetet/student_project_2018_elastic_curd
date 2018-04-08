eac202beac91a6dd08ae680a60700d1955bb4e7f
Max Collin <maxcollin@gmail.com>
2018-04-08 11:29:17 +0200

    Testing node fetch solutions

--- app/index.js
+++ app/index.js
@@ -1,12 +1,55 @@
+// https://github.com/jprichardson/node-jsonfile
+var fetch = require('node-fetch');
+var jsonfile = require('jsonfile');
+var file = '/tmp/data.json'
 
-const calc = require('./calc')
+const ksam = 'http://www.kulturarvsdata.se/ksamsok/api?method=search&stylesheet=stylesheet/searchStyle.xsl&query=item=yxa&place=gotland&startRecord=10&hitsPerPage=25&recordSchema=presentation&x-api=test'
 
-const numbersToAdd = [
-  3,
-  4,
-  10,
-  2
-]
+/* const fetchAsyncA = async (url) => await (await fetch(url)).json();
+ */
+// .result.records.record[1]["pres:item"]["pres:image"]["pres:src"][2].content
 
-const result = calc.sum(numbersToAdd)
-console.log(`The result is: ${result}`)
+
+async function test() {
+
+
+  // console.log(await fetchAsync(ksam));
+  var data = await fetchAsync(ksam);
+  jsonfile.writeFile(file, data, err => console.log(err))
+
+
+
+}
+
+test();
+/* 
+jsonfile.writeFile(file, obj, function (err) {
+  console.error(err)
+}) */
+
+
+async function fetchAsync(url) {
+  return await (await fetch(url, {
+    headers: new Headers({
+      'Accept': 'application/json'
+    })
+  }
+  )).json();
+}
+
+async function timedTest1() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('hello1')
+    }, 2000)
+  })
+}
+
+async function timedTest2() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('hello2')
+    }, 200)
+  })
+}
+
